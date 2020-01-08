f0f62715bd6ab9c2af702015e31e001177f0d790
ljlcarrien <carrientengman@gmail.com>
2020-01-08 16:05:49 +0800

    Add 创建工程 vue init simulatedgreg/electron-vue evtest

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'evtest'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
