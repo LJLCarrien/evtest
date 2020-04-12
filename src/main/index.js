7701fd3957e5dd134167a7192164033a859ccf41
ljlcarrien <ljlcarrien@icloud.com>
2020-04-12 23:54:54 +0800

    Add electron-vue主进程调试 参考 vscode 如何断点调试 electron-vue 程序主进程？ https://newsn.net/say/electron-vue-vscode-debug-main.html

--- src/main/index.js
+++ src/main/index.js
@@ -11,6 +11,7 @@ if (process.env.NODE_ENV !== 'development') {
 }
 
 let mainWindow
+
 const winURL = process.env.NODE_ENV === 'development'
   ? `http://localhost:9080`
   : `file://${__dirname}/index.html`
